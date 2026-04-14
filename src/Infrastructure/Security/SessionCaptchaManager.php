e72ff86e1711097f7898bc201feaae4d9a06025c
Alfred TCHONDJO <altimagni@yahoo.fr>
2026-04-14 22:25:40 +0200

    update : project refactoring and global fix  v3.8

--- src/Infrastructure/Security/SessionCaptchaManager.php
+++ src/Infrastructure/Security/SessionCaptchaManager.php
@@ -20,13 +20,8 @@ final class SessionCaptchaManager implements CaptchaManagerInterface
         $minLength = max(5, $minLength);
         $maxLength = max($minLength, min(8, $maxLength));
 
-        $alphabet = 'ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789';
         $length = random_int($minLength, $maxLength);
-        $code = '';
-
-        for ($i = 0; $i < $length; $i++) {
-            $code .= $alphabet[random_int(0, strlen($alphabet) - 1)];
-        }
+        $code = $this->generateCaseSensitiveCode($length);
 
         $_SESSION['_pfg_captcha'][$key] = $code;
 
@@ -39,7 +34,6 @@ final class SessionCaptchaManager implements CaptchaManagerInterface
             return false;
         }
 
-        /** @var string|null $expected */
         $expected = $_SESSION['_pfg_captcha'][$key] ?? null;
         unset($_SESSION['_pfg_captcha'][$key]);
 
@@ -65,5 +59,26 @@ final class SessionCaptchaManager implements CaptchaManagerInterface
         if (session_status() !== PHP_SESSION_ACTIVE) {
             throw new RuntimeException('Unable to start session for captcha storage.');
         }
+    }
+
+    private function generateCaseSensitiveCode(int $length): string
+    {
+        $uppercase = 'ABCDEFGHJKLMNPQRSTUVWXYZ';
+        $lowercase = 'abcdefghijkmnopqrstuvwxyz';
+        $digits = '23456789';
+        $all = $uppercase . $lowercase . $digits;
+
+        $chars = [
+            $uppercase[random_int(0, strlen($uppercase) - 1)],
+            $lowercase[random_int(0, strlen($lowercase) - 1)],
+        ];
+
+        while (count($chars) < $length) {
+            $chars[] = $all[random_int(0, strlen($all) - 1)];
+        }
+
+        shuffle($chars);
+
+        return implode('', $chars);
     }
 }
